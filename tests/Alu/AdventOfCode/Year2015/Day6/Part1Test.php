4b432b8b99d1300994237463d7117d3d09dcfc2f
alu <alu@byteberry.net>
2021-06-25 19:45:40 +0200

    Solve both parts for Day 6

---
+++ tests/Alu/AdventOfCode/Year2015/Day6/Part1Test.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Alu\AdventOfCode\Year2015\Day6;
+
+use Alu\AdventOfCode\Year2015\Day6\Part1;
+use PHPUnit\Framework\TestCase;
+
+class Part1Test extends TestCase
+{
+    /**
+     * @dataProvider inputProvider
+     */
+    public function testRun($input, $output)
+    {
+        $solution = new Part1();
+        $solution->setInput($input);
+
+        $this->assertSame($output, $solution->run());
+    }
+
+    public function inputProvider(): array
+    {
+        return [
+            ['turn on 0,0 through 999,999', 1000000],
+            ['toggle 0,0 through 999,0', 1000],
+            ['turn off 499,499 through 500,500', 0]
+        ];
+    }
+}
---
+++ tests/Alu/AdventOfCode/Year2015/Day6/Part2Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Alu\AdventOfCode\Year2015\Day6;
+
+use Alu\AdventOfCode\Year2015\Day6\Part2;
+use PHPUnit\Framework\TestCase;
+
+class Part2Test extends TestCase
+{
+    /**
+     * @dataProvider inputProvider
+     */
+    public function testRun($input, $output)
+    {
+        $solution = new Part2();
+        $solution->setInput($input);
+
+        $this->assertSame($output, $solution->run());
+    }
+
+    public function inputProvider(): array
+    {
+        return [
+            ['turn on 0,0 through 0,0', 1],
+            ['toggle 0,0 through 999,999', 2000000]
+        ];
+    }
+}
