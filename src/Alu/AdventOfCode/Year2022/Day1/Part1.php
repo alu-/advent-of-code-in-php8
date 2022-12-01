0122395ac841c22f3ef955a649eb00a2f3831ec6
Oskar Andersson <oskar.andersson@evimeria.se>
2022-12-01 11:52:56 +0100

    Solve day 1 part 1

---
+++ src/Alu/AdventOfCode/Year2022/Day1/Part1.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Alu\AdventOfCode\Year2022\Day1;
+
+use Alu\AdventOfCode\Helpers\{Solution, SolutionInterface};
+
+class Part1 extends Solution implements SolutionInterface
+{
+    public function run(): int
+    {
+        $dwarfs = [];
+        foreach (explode("\n\n",$this->getInput()) as $dwarf) {
+            $dwarfs[] = array_sum(array_map('intval', explode("\n", $dwarf)));
+        }
+
+        return max($dwarfs);
+    }
+}
