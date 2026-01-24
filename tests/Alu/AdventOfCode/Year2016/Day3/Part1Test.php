76c7fb48d67d6634e8cbfd1d201e6528028dd53e
alu- <alu@byteberry.net>
2026-01-24 17:45:14 +0100

    Solve 2016 day 3

---
+++ tests/Alu/AdventOfCode/Year2016/Day3/Part1Test.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Alu\AdventOfCode\Year2016\Day3;
+
+use Alu\AdventOfCode\Year2016\Day3\Part1;
+use Tests\Alu\AdventOfCode\TestCase;
+
+class Part1Test extends TestCase
+{
+    /**
+     * @dataProvider inputProvider
+     */
+    public function testRun($input, $output)
+    {
+        /** @var Part1 $solution */
+        $solution = $this->getMockForPart(Part1::class);
+        $solution->setInput($input);
+
+        $this->assertSame($output, $solution->run());
+    }
+
+    public static function inputProvider(): array
+    {
+
+
+        return [
+            [<<<'EOD'
+  496  369  792
+  677  412  833
+   95  316  802
+  957  774  647
+  966  842  861
+  233  737  194
+  260  605  424
+EOD, 5],
+        ];
+    }
+}
---
+++ tests/Alu/AdventOfCode/Year2016/Day3/Part2Test.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Alu\AdventOfCode\Year2016\Day3;
+
+use Alu\AdventOfCode\Year2016\Day3\Part2;
+use Tests\Alu\AdventOfCode\TestCase;
+
+class Part2Test extends TestCase
+{
+    /**
+     * @dataProvider inputProvider
+     */
+    public function testRun($input, $output)
+    {
+        /** @var Part2 $solution */
+        $solution = $this->getMockForPart(Part2::class);
+        $solution->setInput($input);
+
+        $this->assertSame($output, $solution->run());
+    }
+
+    public static function inputProvider(): array
+    {
+        return [
+            [<<<'EOD'
+  496  369  792
+  677  412  833
+   95  316  802
+  957  774  647
+  966  842  861
+  233  737  194
+EOD, 4],
+        ];
+    }
+}
